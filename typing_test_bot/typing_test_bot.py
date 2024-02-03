72da6837d8c09500d94b9a16768305a12b1eb486
Tanmeet Singh Chhabra <tanmeet.chhabra.0@gmail.com>
2024-02-03 20:18:34 +0530

    created a better way to stop the bot in the middle of typing
    before the whole program abruptly stopped if the typing needed to be stopped
    
    now only the typing is stopped and the rest of the program continues like normal

--- typing_test_bot/typing_test_bot.py
+++ typing_test_bot/typing_test_bot.py
@@ -1,16 +1,22 @@
 import pynput, pytesseract, time, os
 from PIL import Image, ImageGrab
+from threading import Thread
 
 # global variables
-START_STOP_KEY = pynput.keyboard.Key.f2
-EXIT_KEY = (
-    pynput.keyboard.Key.f4
-)  # exit the program after completing the current typing
-FORCE_STOP_KEY = (
-    pynput.keyboard.Key.esc
-)  # just in case the program activates somewhere it's not supposed to
+# press this key to start the bot
+RUN_BOT_KEY = pynput.keyboard.Key.f2
+
+# press this key to exit the program after completing the current typing
+EXIT_KEY = pynput.keyboard.Key.f4
+
+# press this key to stop the bot in the middle of typing
+# just in case the program activates somewhere it's not supposed to
+TERMINATE_TYPING_KEY = pynput.keyboard.Key.esc
+
+# global variables which are modified by multiple threads
 running = True
-start_typing = False
+start_bot = False
+terminate_typing = False
 
 
 def main():
@@ -19,23 +25,19 @@ def main():
     1. the ocr cannot read the word possibility
     2. the ocr randomly interprets the cursor as many different characters like [,l,I ; most common is [
     """
+    global start_bot, terminate_typing
     key_typer = pynput.keyboard.Controller()
+    # the key listener is a separate thread which listens for key presses
+    # when a key is pressed it calls the given on_press function with the key as an argument
     key_listener = pynput.keyboard.Listener(on_press=on_key_press)
     page_up_key = pynput.keyboard.Key.page_up
-    website_reload_key = pynput.keyboard.Key.f5
 
     # the amount of time to wait between typing characters ; in seconds
-    wait_between_characters = 0.02
+    wait_between_characters = 0
 
     key_listener.start()
     while running:
-        if start_typing:
-            # focus on the textbox
-            # when the website is loaded the text box is automatically focused
-            key_typer.tap(website_reload_key)
-            # wait for the webpage to reload
-            time.sleep(1)
-
+        if start_bot:
             # take a screenshot
             screen_shot = get_screen_shot()
             # the text box is the area in the screen shot where all the text that need to be typed is contained
@@ -45,31 +47,37 @@ def main():
                 text = sanitize_text(text)
                 print(text)
                 print()
-                type_text(key_typer, text, wait_between_characters)
+                # start typing text in a separate thread and wait for the typing to complete
+                terminate_typing = False
+                key_typer_thread = Thread(
+                    target=type_text, args=(key_typer, text, wait_between_characters)
+                )
+                key_typer_thread.start()
+                key_typer_thread.join()
 
                 # after the typing is completed the webpage randomly scrolls down sometimes
                 # so to get back to the top of the page press page up
-                # berfore that wait for the webpage to actually go down
-                time.sleep(1.5)
-                key_typer.tap(page_up_key)
-
-            # wait before the next iteration
-            time.sleep(5)
-
+                # before that wait for the webpage to actually go down
+                time.sleep(2)
+                key_typer.press(page_up_key)
+                time.sleep(1)
+                key_typer.release(page_up_key)
+            start_bot = False
     key_listener.stop()
     print("Exiting...")
 
 
 def on_key_press(key):
-    if key == START_STOP_KEY:
-        global start_typing
-        start_typing = not start_typing
+    if key == RUN_BOT_KEY:
+        global start_bot
+        if not start_bot:
+            start_bot = True
     if key == EXIT_KEY:
         global running
         running = False
-    if key == FORCE_STOP_KEY:
-        # stop everything and exit
-        os._exit(0)
+    if key == TERMINATE_TYPING_KEY:
+        global terminate_typing
+        terminate_typing = True
 
 
 def get_screen_shot():
@@ -84,6 +92,7 @@ def get_text_box_from_screen_shot(screen_shot: "Image.Image"):
     # the colour of the text box is different from all the other things in the screenshot
     # this is the colour of the text box when it is focused
     # the text box has a different colour when it is unfocused
+    # this is the colour of the textbox when it is focused
     text_box_colour = (234, 243, 250)  # rgb colour ; hardcoded
     width, height = screen_shot.size
     text_box_top_left = None
@@ -118,14 +127,19 @@ def sanitize_text(text: "str"):
     text = text.replace(
         "  ", " "
     )  # sometimes there are accidental two spaces in a row when there should be one
-    text = text.replace("ona","on a") # the ocr interprets "on a" as a single word "ona", the actual word "ona" will probably not be in the text
+    text = text.replace(
+        "ona", "on a"
+    )  # the ocr interprets "on a" as a single word "ona", the actual word "ona" will probably not be in the text
     return text
 
 
 def type_text(key_typer: "pynput.keyboard.Controller", text, wait_between_characters):
+    # runs in a separate thread so that it can be stopped by pressing a key
     for char in text:
         key_typer.tap(char)
         time.sleep(wait_between_characters)
+        if terminate_typing:
+            break
 
 
 main()
